fc0c78ea379cf2b3f8af48e07c3d8df66e42979c
BrunoSette <brunosette@gmail.com>
2019-08-22 08:35:43 -0300

    Delete comments

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Material Kit React - v1.7.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
@@ -22,7 +5,6 @@ import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.7.0";
 
-// pages for this product
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import HomePage from "views/HomePage/HomePage.jsx";
 import Album from "views/Album/Album.jsx";
